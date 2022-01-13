c977e86d3fefb083ce13464bfbd791ede85dad88
Leonardo Di Donato <leodidonato@gmail.com>
2022-01-13 03:02:34 +0100

    docs(examples): SPDX identifiers for examples
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- examples/src/fentry.bpf.c
+++ examples/src/fentry.bpf.c
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-2.0
+// SPDX-License-Identifier: GPL-2.0-only
 #include "vmlinux.h"
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
--- examples/src/raw_enter.c
+++ examples/src/raw_enter.c
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-2.0
+// SPDX-License-Identifier: GPL-2.0-only
 #include <asm/unistd.h>
 #include <bpf/bpf.h>
 #include "commons.c"
